dcd33e732bbc10ac6b8b3649e08694d01cd3b1f6
Maher-Amara <maheramara32@yahoo.fr>
2022-01-10 14:37:04 +0100

    update

--- src/com/garderie/Main.java
+++ src/com/garderie/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.mainLoop();
-//        School school =new School(1);
-//        school.showElevelist();
     }
 }
